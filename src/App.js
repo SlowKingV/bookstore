b5fa375849cc2c938c1c442d6eb9d669b624bb5f
Diego Luna <slowkingvii@gmail.com>
2023-01-28 00:41:43 -0600

    Add routes to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navigation from './components/Navigation';
+
+const App = () => (
+  <>
+    <Navigation />
+
+    <Routes>
+      <Route path="/" element={<h2>This is the Books page</h2>} />
+      <Route path="/categories" element={<h2>This is the Categories page</h2>} />
+      <Route path="*" element={<h2>404: Page not found</h2>} />
+    </Routes>
+  </>
+);
 
 export default App;
